61b5325734a9d6b67abd2c55dc4cf3c7f02b1c8d
truong250830 <th326080@gmail.com>
2025-06-17 23:20:38 +0700

    sua sidebar admin

--- src/components/admin/orders/Sidebar.jsx
+++ src/components/admin/orders/Sidebar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <aside className="w-[300px] h-[850px] bg-black text-white p-5">
+    <aside className="w-[300px] h-[950px] bg-black text-white p-5">
       <div className="text-2xl text-center font-bold mb-8"><Link
           to="/admin"
           
@@ -59,7 +59,7 @@ const Sidebar = () => {
           Thống kê doanh thu
         </Link>
         <Link
-          to=""
+          to="/"
           className="block px-4 py-2 rounded-3xl text-white transition-all hover:text-[#E65F2B] hover:bg-white hover:ml-4 font-normal hover:font-bold"
         >
           → Trở về trang chủ
