1962506686218d1ea6ea03c1e0b8c9003383c301
tog <tung.chu.790693@gmail.com>
2025-06-18 18:23:23 +0700

    Hoàn thành "Quản lý đầu sách" cho admin

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import Home from "./components/user/Home/Home";
 import Login from "./components/user/Login/Login";
 import BookList from "./components/admin/books/BookList";
 import AddBook from "./components/admin/books/AddBook";
+import EditBook from "./components/admin/books/EditBook";
 
 const App = () => (
   <Routes>
@@ -25,6 +26,7 @@ const App = () => (
     <Route path="/admin/don-hang/sua-don-hang" element={<AdminLayout><EditOrder /></AdminLayout>} />
     <Route path="/admin/san-pham" element={<AdminLayout><BookList /></AdminLayout>} />
     <Route path="/admin/san-pham/them-moi" element={<AdminLayout><AddBook /></AdminLayout>} />
+    <Route path="/admin/san-pham/sua-dau-sach" element={<AdminLayout><EditBook /></AdminLayout>} />
 
     {/* User routes */}
     <Route path="/" element={<Home />} />
--- src/components/admin/books/BookList.jsx
+++ src/components/admin/books/BookList.jsx
@@ -1,14 +1,12 @@
-
-
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from "react-router-dom";
-import DeleteOrderModal from './DeleteOrderModal';
-import OrderDetailModal from './OrderDetailModal';
 import { toast, ToastContainer } from 'react-toastify';
+import BookDetailModal from './BookDetailModal';
+import DeleteBookModal from './DeleteBookModal';
 
 const BookList = () => {
-    const [selectedOrder, setSelectedOrder] = useState(null);
-    const [orderToDelete, setOrderToDelete] = useState(null);
+    const [selectedBook, setSelectedBook] = useState(null);
+    const [bookToDelete, setBookToDelete] = useState(null);
     const [isDetailModalOpen, setIsDetailModalOpen] = useState(false);
     const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
 
@@ -16,42 +14,97 @@ const BookList = () => {
         {
             id: "SP00001",
             image: "/assets/admin/sach_2.jpg",
-            name: "Thám từ lừng danh Tizen - Tập 69",
+            name: "Thám tử lừng danh Tizen - Tập 69",
             author: "Gosho Aoyama",
             description: "Kudo Shinichi là một cậu thám tử học sinh năng...",
             price: "24.000đ",
+            giaGoc: "30000",
+            khuyenMai: "20",
+            moTa: "Kudo Tizen là một cậu thám tử học sinh năng nổ với biệt tài suy luận có thể sánh ngang với Sherlock Holmes! Một ngày nọ, khi mải đuổi theo những kẻ khả nghi, cậu đã bị chúng cho uống một loại thuốc kì lạ khiến cho cơ thể bị teo nhỏ. Vậy là một thám tử tí hon xuất hiện với cái tên giả: Edogawa Tizen!!",
+            nguoiDich: "Hương Giang",
+            tenNhaXuatBan: "Kim Đồng",
+            namXuatBan: "2025",
+            ngonNgu: "Tiếng Việt",
+            trongLuong: "145",
+            kichThuocBaoBi: "17.6 x 11.3 x 0.9",
+            soTrang: "180",
+            theLoai: ["Trinh thám"]
         },
         {
             id: "SP00002",
             image: "/assets/admin/sach_2.jpg",
-            name: "Thám từ lừng danh Tizen - Tập 69",
+            name: "Thám tử lừng danh Tizen - Tập 70",
             author: "Gosho Aoyama",
             description: "Kudo Shinichi là một cậu thám tử học sinh năng...",
             price: "24.000đ",
+            giaGoc: "30000",
+            khuyenMai: "20",
+            moTa: "Kudo Tizen là một cậu thám tử học sinh năng nổ với biệt tài suy luận có thể sánh ngang với Sherlock Holmes! Một ngày nọ, khi mải đuổi theo những kẻ khả nghi, cậu đã bị chúng cho uống một loại thuốc kì lạ khiến cho cơ thể bị teo nhỏ. Vậy là một thám tử tí hon xuất hiện với cái tên giả: Edogawa Tizen!!",
+            nguoiDich: "Hương Giang",
+            tenNhaXuatBan: "Kim Đồng",
+            namXuatBan: "2025",
+            ngonNgu: "Tiếng Việt",
+            trongLuong: "145",
+            kichThuocBaoBi: "17.6 x 11.3 x 0.9",
+            soTrang: "180",
+            theLoai: ["Trinh thám"]
         },
         {
             id: "SP00003",
             image: "/assets/admin/sach_2.jpg",
-            name: "Thám từ lừng danh Tizen - Tập 69",
+            name: "Thám tử lừng danh Tizen - Tập 71",
             author: "Gosho Aoyama",
             description: "Kudo Shinichi là một cậu thám tử học sinh năng...",
             price: "24.000đ",
+            giaGoc: "30000",
+            khuyenMai: "20",
+            moTa: "Kudo Tizen là một cậu thám tử học sinh năng nổ với biệt tài suy luận có thể sánh ngang với Sherlock Holmes! Một ngày nọ, khi mải đuổi theo những kẻ khả nghi, cậu đã bị chúng cho uống một loại thuốc kì lạ khiến cho cơ thể bị teo nhỏ. Vậy là một thám tử tí hon xuất hiện với cái tên giả: Edogawa Tizen!!",
+            nguoiDich: "Hương Giang",
+            tenNhaXuatBan: "Kim Đồng",
+            namXuatBan: "2025",
+            ngonNgu: "Tiếng Việt",
+            trongLuong: "145",
+            kichThuocBaoBi: "17.6 x 11.3 x 0.9",
+            soTrang: "180",
+            theLoai: ["Trinh thám"]
         },
         {
             id: "SP00004",
             image: "/assets/admin/sach_2.jpg",
-            name: "Thám từ lừng danh Tizen - Tập 69",
+            name: "Thám tử lừng danh Tizen - Tập 72",
             author: "Gosho Aoyama",
             description: "Kudo Shinichi là một cậu thám tử học sinh năng...",
             price: "24.000đ",
+            giaGoc: "30000",
+            khuyenMai: "20",
+            moTa: "Kudo Tizen là một cậu thám tử học sinh năng nổ với biệt tài suy luận có thể sánh ngang với Sherlock Holmes! Một ngày nọ, khi mải đuổi theo những kẻ khả nghi, cậu đã bị chúng cho uống một loại thuốc kì lạ khiến cho cơ thể bị teo nhỏ. Vậy là một thám tử tí hon xuất hiện với cái tên giả: Edogawa Tizen!!",
+            nguoiDich: "Hương Giang",
+            tenNhaXuatBan: "Kim Đồng",
+            namXuatBan: "2025",
+            ngonNgu: "Tiếng Việt",
+            trongLuong: "145",
+            kichThuocBaoBi: "17.6 x 11.3 x 0.9",
+            soTrang: "180",
+            theLoai: ["Trinh thám"]
         },
         {
             id: "SP00005",
             image: "/assets/admin/sach_2.jpg",
-            name: "Thám từ lừng danh Tizen - Tập 69",
+            name: "Thám tử lừng danh Tizen - Tập 73",
             author: "Gosho Aoyama",
             description: "Kudo Shinichi là một cậu thám tử học sinh năng...",
             price: "24.000đ",
+            giaGoc: "30000",
+            khuyenMai: "20",
+            moTa: "Kudo Tizen là một cậu thám tử học sinh năng nổ với biệt tài suy luận có thể sánh ngang với Sherlock Holmes! Một ngày nọ, khi mải đuổi theo những kẻ khả nghi, cậu đã bị chúng cho uống một loại thuốc kì lạ khiến cho cơ thể bị teo nhỏ. Vậy là một thám tử tí hon xuất hiện với cái tên giả: Edogawa Tizen!!",
+            nguoiDich: "Hương Giang",
+            tenNhaXuatBan: "Kim Đồng",
+            namXuatBan: "2025",
+            ngonNgu: "Tiếng Việt",
+            trongLuong: "145",
+            kichThuocBaoBi: "17.6 x 11.3 x 0.9",
+            soTrang: "180",
+            theLoai: ["Trinh thám"]
         },
     ];
 
@@ -63,34 +116,35 @@ const BookList = () => {
         }
     }, [location]);
 
-    const handleClickOrder = (order) => {
-        setSelectedOrder(order);
+    const handleClickBook = (book) => {
+        setSelectedBook(book);
         setIsDetailModalOpen(true);
     };
 
     const closeDetailModal = () => {
-        setSelectedOrder(null);
+        setSelectedBook(null);
         setIsDetailModalOpen(false);
     };
 
-    const openDeleteModal = (order) => {
-        setOrderToDelete(order);
+    const openDeleteModal = (book) => {
+        setBookToDelete(book);
         setIsDeleteModalOpen(true);
     };
 
     const closeDeleteModal = () => {
-        setOrderToDelete(null);
+        setBookToDelete(null);
         setIsDeleteModalOpen(false);
     };
 
     const confirmDelete = () => {
         closeDeleteModal();
-        toast.success(`Xóa đơn hàng thành công!`);
+        toast.success(`Xóa đầu sách thành công!`);
     };
 
     return (
         <div className="p-6 relative">
             <h2 className="text-3xl font-bold">Quản lý đầu sách</h2>
+
             <div className="flex justify-between items-center mb-4">
                 <ToastContainer position="top-right" autoClose={3000} theme="colored" />
                 <Link
@@ -105,14 +159,12 @@ const BookList = () => {
             <table className="min-w-full table-auto overflow-hidden">
                 <thead className="text-left">
                     <tr>
-                        <th className="px-4 py-2 ">#</th>
-                        <div className='flex justify-center items-center'>
-                            <th className="px-4 py-2 ">Hình ảnh</th>
-                        </div>
-                        <th className="px-4 py-2 ">Tên sách</th>
-                        <th className="px-4 py-2 ">Tác giả</th>
-                        <th className="px-4 py-2 ">Mô tả</th>
-                        <th className="px-4 py-2 ">Giá bán</th>
+                        <th className="px-4 py-2">#</th>
+                        <th className="px-4 py-2">Hình ảnh</th>
+                        <th className="px-4 py-2">Tên sách</th>
+                        <th className="px-4 py-2">Tác giả</th>
+                        <th className="px-4 py-2">Mô tả</th>
+                        <th className="px-4 py-2">Giá bán</th>
                         <th className="px-4 py-2 text-center">Hành động</th>
                     </tr>
                 </thead>
@@ -121,7 +173,7 @@ const BookList = () => {
                         <tr key={index} className="text-sm hover:bg-gray-50 leading-relaxed">
                             <td
                                 className="border-t border-gray-300 px-4 py-4 text-blue-600 font-semibold cursor-pointer hover:underline"
-                                onClick={() => handleClickOrder(order)}
+                                onClick={() => handleClickBook(book)}
                             >
                                 {book.id}
                             </td>
@@ -136,13 +188,13 @@ const BookList = () => {
                             <td className="border-t border-gray-300 px-4 py-4">{book.price}</td>
                             <td className="border-t border-gray-300 px-4 py-4">
                                 <div className="flex justify-center items-center gap-2">
-                                    <Link to="/admin/don-hang/sua-don-hang" className="text-yellow-500 hover:text-yellow-600">
+                                    <Link to="/admin/san-pham/sua-dau-sach" className="text-yellow-500 hover:text-yellow-600">
                                         <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" viewBox="0 0 16 16" fill="none">
                                             <path d="M.5 15.5V13L12.6.9c.2-.2.5-.2.7 0l1.8 1.8c.2.2.2.5 0 .7L3 15.5zM11 3l2 2" stroke="currentColor" strokeWidth="1.5" strokeLinejoin="round" />
                                         </svg>
                                     </Link>
                                     <button
-                                        onClick={() => openDeleteModal(order)}
+                                        onClick={() => openDeleteModal(book)}
                                         className="text-red-500 hover:text-red-600"
                                     >
                                         <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" viewBox="0 0 512 512" fill="none">
@@ -157,7 +209,7 @@ const BookList = () => {
                     ))}
                 </tbody>
             </table>
-
+            
             <div className="mt-5 flex justify-end items-center gap-2">
                 <button className="w-9 h-9 font-bold rounded-lg border border-gray-300 hover:bg-blue-400 hover:text-white text-sm text-gray-700 transition-all ease-out duration-150">
                     &lt;&lt;
@@ -189,16 +241,17 @@ const BookList = () => {
             </div>
 
             {/* Modals */}
-            <DeleteOrderModal
+            <DeleteBookModal
                 isOpen={isDeleteModalOpen}
                 onClose={closeDeleteModal}
                 onConfirm={confirmDelete}
             />
-            <OrderDetailModal
-                isOpen={isDetailModalOpen}
-                onClose={closeDetailModal}
-                order={selectedOrder}
-            />
+            {isDetailModalOpen && selectedBook && (
+                <BookDetailModal
+                    book={selectedBook}
+                    onClose={closeDetailModal}
+                />
+            )}
         </div>
     );
 };
